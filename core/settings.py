741072d6729d8ece2b6925e54bd63dda6a67880f
Renato Silva <renato.augusto.correa@gmail.com>
2024-02-13 23:03:17 -0300

    Acertando rota de static files

--- core/settings.py
+++ core/settings.py
@@ -114,7 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
